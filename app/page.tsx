56510384762cda7278a890ebb73a30533c05d5bf
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-09-21 18:01:55 +0530

    setup project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello!!! And Welcome for your new project
+  return <div>
+    <Button variant={"destructive"}>Hello!!! And Welcome for your new project</Button>
+    
     </div>; 
 }
