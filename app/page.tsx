34121b152ab6128477199d0dc9e5faa273c5e5a6
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-09-22 17:43:34 +0530

    refactor Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>
+  return (
+  <div className="pt-40">
     <Button variant={"destructive"}>Hello!!! And Welcome for your new project</Button>
     
-    </div>; 
+    </div>
+    ) 
 }
