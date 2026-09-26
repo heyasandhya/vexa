8e95d1b4f4ecc0e111f44e9eb2f83ca70e6838c6
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-09-26 16:18:33 +0530

    add PricingModal component

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { ArrowRight, Ghost, Zap } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from './ui/button'
+import PricingModal from './PricingModal'
 
 
 const Header = () => {
@@ -25,9 +26,11 @@ const Header = () => {
 						Projects
 					</Link>
 
+		<PricingModal>
 					<span className='inline-flex h-8 items-center gap-1.5 rounded-full border border-white-10 bg-white/5 px-3 text-xs text-white/70'>
 						<Zap className='h-3 w-3 fill-white/70'></Zap>
 						3 / 40 credits</span>
+						</PricingModal>
 					<UserButton />
 				</Show>
 
